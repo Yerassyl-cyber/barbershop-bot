40fe7baac1c3301e8744797c421f2b33f538ad97
yerassyl <yerassyl@gmail.com>
2026-02-12 16:02:33 +0500

    Save bookings to Azure SQL + slot check

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 
 BOT_TOKEN = os.getenv("BARBER_BOT_TOKEN", "")
 WEBHOOK_SECRET = os.getenv("BARBER_WEBHOOK_SECRET", "")
-
+SQL_CONN_STR = os.getenv("SQL_CONN_STR", "")
 TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}"
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
